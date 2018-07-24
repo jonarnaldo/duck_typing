ed5552aeeb6f3b2a46cfe1230df797e266a089e8
jon arnaldo <jonarnaldo@gmail.com>
2018-07-24 15:58:48 -0700

    adds document folder

--- document.sh
+++ document.sh
@@ -13,30 +13,37 @@ echo 'updating documentation'
 
 git clone https://github.com/jonarnaldo/duck_typing.git
 
-cd duck_type
-
 echo $pwd
 
-# # set variables if they don't exist
-# ${DOCUMENTATION:=$HOME/workspace/meteor/packages/Documentation/}
-# ${UNITY_COMPONENTS:=$HOME/workspace/unity/app/imports/client/ui/components/*}
-#
-# # overwrite readme file with updated info
-# echo "# Documentation"$'\r'$'\r' > $DOCUMENTATION/README.md
-# echo "## Unity Component Table of Contents" >> $DOCUMENTATION/README.md
-#
-# # writes jsdoc console output to markdown file
-# write_markdown_file () {
-#   local fileName
-#   fileName=$(basename $1)
-#   DesName=$(sed s/jsx/md/g <<< ${fileName})
-#   jsdoc2md $1 > $DOCUMENTATION${DesName}
-#   echo "writing file ${DesName} to ${DOCUMENTATION}"
-#
-#   # create appropriate link in readme
-#   echo "* [${fileName}](${DesName})" >> $DOCUMENTATION/README.md
-# }
-#
-# export -f write_markdown_file
-#
-# find $UNITY_COMPONENTS -type f -name '*.jsx' ! -name '*.test.jsx' -exec bash -c 'write_markdown_file "$1"' - {} \;
+# set variables if they don't exist
+${DOCUMENTATION:=$PWD/documentation}
+${COMPONENTS:=$PWD/src/*}
+SHA=`git rev-parse --verify HEAD`
+
+# overwrite readme file with updated info
+echo "# Documentation"$'\r'$'\r' > $DOCUMENTATION/document.md
+echo "## Component Table of Contents" >> $DOCUMENTATION/document.md
+
+# writes jsdoc console output to markdown file
+write_markdown_file () {
+  local fileName
+  fileName=$(basename $1)
+  DesName=$(sed s/js/md/g <<< ${fileName})
+  jsdoc2md $1 > $DOCUMENTATION${DesName}
+  echo "writing file ${DesName} to ${DOCUMENTATION}"
+
+  # create appropriate link in readme
+  echo "* [${fileName}](${DesName})" >> $DOCUMENTATION/document.md
+}
+
+export -f write_markdown_file
+
+find $COMPONENTS -type f -name '*.jsx' ! -name '*.test.jsx' -exec bash -c 'write_markdown_file "$1"' - {} \;
+
+# Commit the "changes", i.e. the new version.
+# The delta will show diffs between new and old versions.
+git add .
+git commit -m "Deploy to GitHub Pages: ${SHA}"
+
+# Now that we're all set up, we can push.
+git push origin master
